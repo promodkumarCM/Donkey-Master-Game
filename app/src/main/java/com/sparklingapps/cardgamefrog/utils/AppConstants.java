42e11e415c17b61a823200264ff0b9cacaf936d0
basiljose <basil1112@gmail.com>
2020-05-13 20:50:02 +0530

    chnaged base url

--- app/src/main/java/com/sparklingapps/cardgamefrog/utils/AppConstants.java
+++ app/src/main/java/com/sparklingapps/cardgamefrog/utils/AppConstants.java
@@ -7,6 +7,6 @@ public class AppConstants {
     public static final String PLAYER_ROOM = "player_room";
     public static final String PLAYER_REGISTER = "player_register";
 
-    public static String baseURL ="http://192.168.1.77:3000/";
-    //public static String baseURL = "http://3.14.80.186:3000/";
+    //public static String baseURL ="http://192.168.1.77:3000/";
+    public static String baseURL = "http://3.14.80.186:3000/";
 }
